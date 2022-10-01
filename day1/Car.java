9af40770449c1573c0faec89f1c1e10301523df1
haripriya-1238 <114815938+haripriya-1238@users.noreply.github.com>
2022-10-01 20:55:32 +0530

    Add files via upload
    Day1 assignments

---
+++ day1/Car.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Car {
+	
+	int carno = 2365221;
+	byte speed = 120;
+	float engine = 255f;
+	short wheel = 34;
+	long price = 51155555255l;
+	char  color ='b';
+	double distance =2365522;
+	boolean license =true;
+	String carname ="Honda";
+	
+	
+	
+
+}
---
+++ day1/Factorial.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Factorial {
+ public static void main(String[] args) {
+	 int a = 5;
+	 int fact =1;
+		 for (int i = 1; i <=a; i++) {
+			 fact=fact*i;
+			
+		}
+		 
+	System.out.println("the factorial" +  " is " +fact );
+}
+
+}
+
+////* What are my learnings from this code?
+//		 * 1) Got to know what is the usage of for loop and it's syntax then about Pre-decrement & Post-decrement
+//		 * 2) Had an idea on for loop now
+//		 * 3) Able to write codes
+
