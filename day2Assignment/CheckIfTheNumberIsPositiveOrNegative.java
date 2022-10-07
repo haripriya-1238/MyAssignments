ba537cceca873cf9b5d01a06d8b163480408dfac
haripriya-1238 <114815938+haripriya-1238@users.noreply.github.com>
2022-10-07 11:14:24 +0530

    Add files via upload

---
+++ day2Assignment/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+	
+public static void main(String[] args) {
+	
+	int number = 35;
+	if (number>=25)
+		System.out.println("The number is positive");
+	else if(number<25)
+	System.out.println("The number is negative");
+	else
+		System.out.println("The number is neither positive nor negative");
+		
+	//Problem statement: Check if the number is positive or negative
+	/*Pseudocode
+	 * : ---------- 
+	 * 1. Initialize a number, say, int number= 35; 
+	 * 
+	 * 2. If a number is positive, print "The number is positive" 
+	 * 
+	 * If a number is negative, print "The number is negative" 
+	 * 
+	 * If it nether negative nor positive, print as
+	 * 
+	 * "The number is neither positive nor negative"
+	 */
+}
+}
